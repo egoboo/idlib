a180a0f5f1838dd741d763c315a1c767ef3f09c6
Michael Heilmann <michaelheilmann@primordialmachine.com>
2018-01-13 10:48:38 +0100

    file system: Rewrite is_directory() using status()

--- library/src/idlib/file_system/is_directory.cpp
+++ library/src/idlib/file_system/is_directory.cpp
@@ -1,16 +1,10 @@
 #include "idlib/file_system/is_directory.hpp"
 
-#if defined (ID_WINDOWS)
-	#include "idlib/file_system/is_directory_windows.hpp"
-#elif defined (ID_POSIX)
-	#include "idlib/file_system/is_directory_posix.hpp"
-#else
-    #error("operating system not supported")	
-#endif
+#include "idlib/file_system/status.hpp"
 
 #include "idlib/file_system/header.in"
 
-bool is_directory(const std::string& filename)
-{ return is_directory_impl(filename); }
+bool is_directory(const std::string& pathname)
+{ return file_type::directory == status(pathname).type(); }
 
 #include "idlib/file_system/footer.in"
--- library/src/idlib/file_system/is_directory.hpp
+++ library/src/idlib/file_system/is_directory.hpp
@@ -5,8 +5,8 @@
 #include "idlib/file_system/header.in"
 
 /// @brief Get if a file is is an existing directory file.
-/// @param filename the filename
+/// @param pathname the pathname
 /// @return @a true if a file is an existing directory file, @a false otherwise
-bool is_directory(const std::string& filename);
+bool is_directory(const std::string& pathname);
 
 #include "idlib/file_system/footer.in"
