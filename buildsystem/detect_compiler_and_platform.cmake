be2af4745b84e6db254ed22e4ac5f354c2a1d802
Michael Heilmann <michaelheilmann@primordialmachine.com>
2018-02-10 21:48:46 +0100

    Update buildsystem

---
+++ buildsystem/detect_compiler_and_platform.cmake
@@ -0,0 +1,107 @@
+# Detect compiler and platform detection.
+#
+# Provides the following numeric constants:
+# - IDLIB_CXX_COMPILER_ID_UNKNOWN
+# - IDLIB_CXX_COMPILER_ID_CLANG
+# - IDLIB_CXX_COMPILER_ID_GCC
+# - IDLIB_CXX_COMPILER_ID_CLANG_MSVC
+#
+# If compiler detection succeeds, define IDLIB_CXX_COMPILER_ID to IDLIB_CXX_COMPILER_ID_CLANG, IDLIB_CXX_COMPILER_ID_GCC, or IDLIB_CXX_COMPILER_ID_CLANG_MSVC.
+# define IDLIX_CXX_COMPILER_NAME to IDLIB_CXX_COMPILER_ID_UNKNOWN otherwise.
+# Defines macro idlib_report_cxx_compiler_id to report the value of IDLIB_CXX_COMPILER_ID.
+#
+#
+# Provides the following numeric constants:
+# - IDLIB_PLATFORM_ID_UNKNOWN
+# - IDLIB_PLATFORM_ID_X64
+# - IDLIB_PLATFORM_ID_X86
+#
+# If platform detection succeeds, define IDLIB_PLATFORM_ID to IDLIB_PLATFORM_ID_X64 or IDLIB_PLATFORM_ID_X86.
+# Define IDLIB_PLATFORM_ID to IDLIB_PLATFORM_ID_UNKNOWN otherwise.
+# Define macro idlib_report_platform_id to report the value of IDLIB_PLATFORM_ID.
+#
+# If multi target detection succeeds, define IDLIB_IS_MULTI_TARGET_GENERATOR to YES.
+# Define IDLIB_IS_MULTI_TARGET_GENERATOR to NO otherwise.
+# Define macro idlib_report_is_multi_target_generator to report the value of IDLIB_IS_MULTI_TARGET_GENERATOR.
+
+	# Compiler name detection.
+
+	set(IDLIB_CXX_COMPILER_ID_UNKNOWN 0)
+	set(IDLIB_CXX_COMPILER_ID_CLANG 1)
+	set(IDLIB_CXX_COMPILER_ID_MSVC 2)
+	set(IDLIB_CXX_COMPILER_ID_GCC 3)
+	
+	if (CMAKE_CXX_COMPILER MATCHES ".*clang")
+	  #message("compiler `CLANG` detected")
+	  set(IDLIB_CXX_COMPILER_ID ${IDLIB_CXX_COMPILER_ID_CLANG})
+	endif()
+
+	if (${CMAKE_CXX_COMPILER_ID} STREQUAL GNU)
+	  #message("compiler `GCC` detected")
+	  set(IDLIB_CXX_COMPILER_ID ${IDLIB_CXX_COMPILER_ID_GCC})
+	endif()
+
+	if (${CMAKE_CXX_COMPILER_ID} STREQUAL MSVC)
+	  message("compiler `MSVC` detected")
+	  set(IDLIB_CXX_COMPILER_ID ${IDLIB_CXX_COMPILER_ID_MSVC})
+	endif()
+
+	macro(idlib_report_cxx_compiler_id)
+	  if (${IDLIB_CXX_COMPILER_ID} EQUAL ${IDLIB_CXX_COMPILER_ID_CLANG})
+	    message("  - Idlib CXX Compiler Id: CLANG")
+	  elseif (${IDLIB_CXX_COMPILER_ID} EQUAL ${IDLIB_CXX_COMPILER_ID_GCC})
+	    message("  - Idlib CXX Compiler Id: GCC")
+	  elseif (${IDLIB_CXX_COMPILER_ID} EQUAL ${IDLIB_CXX_COMPILER_ID_MSVC})
+	    message("  - Idlib CXX Compiler Id: MSVC")
+      elseif (${IDLIB_CXX_COMPILER_ID} EQUAL ${IDLIB_CXX_COMPILER_ID_UNKNOWN})
+ 	    message("  - Idlib CXX Compiler Id: Unknown")
+      else()
+	    message(FATAL_ERROR "unhandled enumeration element")
+	  endif()
+	endmacro()
+
+	# Platform name detection.
+
+	set(IDLIB_PLATFORM_ID_UNKNOWN 0)
+	set(IDLIB_PLATFORM_ID_X64 1)
+	set(IDLIB_PLATFORM_ID_X86 2)
+
+	if( CMAKE_SIZEOF_VOID_P EQUAL 8 )
+		#message("platform `x64` detected")
+		set(IDLIB_PLATFORM_ID ${IDLIB_PLATFORM_ID_X64})
+	elseif ( CMAKE_SIZEOF_VOID_P EQUAL 4)
+		#message("platform `x86` detected" )
+		set(IDLIB_PLATFORM_ID ${IDLIB_PLATFORM_ID_X86})
+	endif()
+
+	macro(idlib_report_platform_id)
+	  if (${IDLIB_PLATFORM_ID} EQUAL ${IDLIB_PLATFORM_ID_X64})
+	    message("  - Idlib Platform Id: x64")
+	  elseif (${IDLIB_PLATFORM_ID} EQUAL ${IDLIB_PLATFORM_ID_X86})
+	    message("  - Idlib Platform Id: x86")
+	  elseif (${IDLIB_PLATFORM_ID} EQUAL ${IDLIB_PLATFORM_ID_UNKNOWN})
+	    message("  - Idlib Platform Id: Unknown")
+      else()
+	    message(FATAL_ERROR "unhandled enumeration element")
+	  endif()
+	endmacro()
+	
+	# Multi target generator detection.
+
+	if (${IDLIB_CXX_COMPILER_ID} EQUAL ${IDLIB_CXX_COMPILER_ID_MSVC})
+		set(IDLIB_IS_MULTI_TARGET_GENERATOR YES)
+	else()
+		set(IDLIB_IS_MULTI_TARGET_GENERATOR NO)   
+	endif()
+
+	macro(idlib_report_is_multi_target_generator)
+		if (IDLIB_IS_MULTI_TARGET_GENERATOR)
+		  message("  - Idlib Is Multi Target Generator: Yes")
+		else()
+		  message("  - Idlib Is Multi Target Generator: NO")
+		endif()
+	endmacro()
+
+	idlib_report_cxx_compiler_id()
+	idlib_report_platform_id()
+    idlib_report_is_multi_target_generator()
--- buildsystem/set_project_default_properties.cmake
+++ buildsystem/set_project_default_properties.cmake
@@ -1,37 +1,32 @@
-# == MSVC character encoding ==================================================================== #
-
-if (${CMAKE_CXX_COMPILER_ID} STREQUAL MSVC)
-  add_definitions(-DUNICODE -D_UNICODE)
-endif()
-
-# == x86 / x64 detection ======================================================================== #
-
-# This way it can be detected wether the target is x86 or x64.
-# TODO: Isn't there a more semantic way to do this.
-if( CMAKE_SIZEOF_VOID_P EQUAL 8 )
-    MESSAGE( "64 bits compiler detected" )
-    SET( EX_PLATFORM 64 )
-	# The name of the platform as used in pathnames.
-    SET( EX_PLATFORM_NAME "x64" )
-elseif( CMAKE_SIZEOF_VOID_P EQUAL 4 ) 
-    MESSAGE( "32 bits compiler detected" )
-    SET( EX_PLATFORM 32 )
-	# The name of the platform as used in pathnames.
-    SET( EX_PLATFORM_NAME "x86" )
-endif( CMAKE_SIZEOF_VOID_P EQUAL 8 )
+include(${CMAKE_CURRENT_LIST_FILE}/../detect_compiler_and_platform.cmake)
 
 # == Doxygen ==================================================================================== #
 
-# CMake 3.10 fails for no reason on FIND_PACKAGE(Doxygen).
-# Hence Doxygen support is disabled for now.
-#FIND_PACKAGE(Doxygen)
+# Find Doxygen.
+# Remark: CMake 3.10 fails under Cygwin for no reason on FIND_PACKAGE(Doxygen).
+FIND_PACKAGE(Doxygen)
+IF(NOT DOXYGEN_FOUND)
+	MESSAGE(STATUS "Doxygen not found. You won't be able to build documentation.")
+ENDIF()
+
+# == Default project settings =================================================================== #
 
 # Macro adjusting (mostly compiler-specific) properties of a project.
 macro(set_project_default_properties)
-	# (1) GNU GCC specific settings
-	# TODO: Raise an error if the MSVC version is too low.
-	if (${CMAKE_CXX_COMPILER_ID} STREQUAL GNU)
-	  message("compiler is GNUCC")
+	if (${IDLIB_CXX_COMPILER_ID} EQUAL ${IDLIB_CXX_COMPILER_ID_MSVC})
+	  message("case of MSVC: add_definitions(-DUNICODE -D_UNICODE)")
+	  add_definitions(-DUNICODE -D_UNICODE)
+	endif()
+
+	# Non-MSVC-specific settings: The project compiles against C++ 17.
+	if (NOT ${IDLIB_CXX_COMPILER_ID} EQUAL ${IDLIB_CXX_COMPILER_ID_MSVC})
+	  message("case of NOT MSVC: set(CMAKE_CXX_STANDARD 17)")
+	  set(CMAKE_CXX_STANDARD 17)
+	endif()
+
+	# GNU GCC specific settings
+	if (${IDLIB_CXX_COMPILER_ID} EQUAL ${IDLIB_CXX_COMPILER_ID_GCC})
+	  message("case of GCC")
 	  # Set standard to C++ 17, enable -Wall and -Wextra
 	  add_definitions("-std=gnu++17 -Wall -Wextra -D_GNU_SOURCE")
 	  # Disable some warnings
@@ -45,8 +40,9 @@ macro(set_project_default_properties)
 
     # (2) MSVC specific settings
     # TODO: Raise an error if the MSVC version is too low.
-    if (${CMAKE_CXX_COMPILER_ID} STREQUAL MSVC)
-      if (MSVC_VERSION GREATER_EQUAL "1900")
+    if (${IDLIB_CXX_COMPILER_ID} EQUAL ${IDLIB_CXX_COMPILER_ID_MSVC})
+      message("case of MSVC")
+	  if (MSVC_VERSION GREATER_EQUAL "1900")
         include(CheckCXXCompilerFlag)
         CHECK_CXX_COMPILER_FLAG("/std:c++latest" _cpp_latest_flag_supported)
         if (_cpp_latest_flag_supported)
@@ -56,8 +52,8 @@ macro(set_project_default_properties)
     endif()
 
 	# (2) MSVC specific settings
-	if (${CMAKE_CXX_COMPILER_ID} STREQUAL MSVC)
-	  message("compiler is MSVC")
+	if (${IDLIB_CXX_COMPILER_ID} EQUAL ${IDLIB_CXX_COMPILER_ID_MSVC})
+	  message("case of MSVC")
 	  set(variables
 		  CMAKE_C_FLAGS_DEBUG
 		  CMAKE_C_FLAGS_MINSIZEREL
@@ -76,28 +72,77 @@ macro(set_project_default_properties)
 	endif()
 
 	# (3) MSVC specific settings
-	if (${CMAKE_CXX_COMPILER_ID} STREQUAL MSVC)
+	if (${IDLIB_CXX_COMPILER_ID} EQUAL ${IDLIB_CXX_COMPILER_ID_MSVC})
+	    message("case of MSVC")
 		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc")
 	endif()
 endmacro(set_project_default_properties)
 
-# Adjust the archive output directory.
-set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/products/${EX_CONFIGURATION_NAME}/${EX_PLATFORM_NAME}/lib)
+# For configuring the output paths, we distinguish between single- and multi-target generators.
+if (IDLIB_IS_MULTI_TARGET_GENERATOR)
+    #message("multi-target generator")
+	# For multi-target generators like MSVC, we specify disjoint output paths for targets.
+	foreach( OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES} )
+		# Suffix for CMake's configuration type specific output path variables.
+		string( TOUPPER ${OUTPUTCONFIG} OUTPUTCONFIG )
+		# The name of the output configuration as used in the pathnames.
+		string( TOLOWER ${OUTPUTCONFIG} OUTPUTCONFIG_FOLDER_NAME)
+		# The name of the output platform as used in the pathnames.
+	    if (${IDLIB_PLATFORM_ID} EQUAL ${IDLIB_PLATFORM_ID_X64})
+	       set(OUTPUTPLATFORM_FOLDER_NAME "x64")
+		elseif (${IDLIB_PLATFORM_ID} EQUAL ${IDLIB_PLATFORM_ID_X86})
+		   set(OUTPUTPLATFORM_FOLDER_NAME "x86")
+		else()
+	      message(FATAL_ERROR "unhandled enumeration element")		
+		endif()
+	    message("  - Idlib Platform Id: x86")
+		message("  - path for CMAKE_RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG} is")
+		message("    ${CMAKE_BINARY_DIR}/products/${OUTPUTCONFIG_FOLDER_NAME}/${OUTPUTPLATFORM_FOLDER_NAME}/bin")
+		set( CMAKE_RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${CMAKE_BINARY_DIR}/products/${OUTPUTCONFIG_FOLDER_NAME}/${OUTPUTPLATFORM_FOLDER_NAME}/bin )
+		message("  - path for CMAKE_LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} is")
+		message("    ${CMAKE_BINARY_DIR}/products/${OUTPUTCONFIG_FOLDER_NAME}/${OUTPUTPLATFORM_FOLDER_NAME}/lib")
+		set( CMAKE_LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${CMAKE_BINARY_DIR}/products/${OUTPUTCONFIG_FOLDER_NAME}/${OUTPUTPLATFORM_FOLDER_NAME}/lib )
+		message("  - path for CMAKE_ARCHIVE_OUTPUT_DIRECTORY_${OUTPUTCONFIG} is")
+		message("    ${CMAKE_BINARY_DIR}/products/${OUTPUTCONFIG_FOLDER_NAME}/${OUTPUTPLATFORM_FOLDER_NAME}/lib")
+		set( CMAKE_ARCHIVE_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${CMAKE_BINARY_DIR}/products/${OUTPUTCONFIG_FOLDER_NAME}/${OUTPUTPLATFORM_FOLDER_NAME}/lib )
+	endforeach( OUTPUTCONFIG CMAKE_CONFIGURATION_TYPES )
+else()
+    #message("single-target generator")
+	
+	# The name of the output platform as used in the pathnames.
+	if (${IDLIB_PLATFORM_ID} EQUAL ${IDLIB_PLATFORM_ID_X64})
+	   set(OUTPUTPLATFORM_FOLDER_NAME "x64")
+	elseif (${IDLIB_PLATFORM_ID} EQUAL ${IDLIB_PLATFORM_ID_X86})
+	   set(OUTPUTPLATFORM_FOLDER_NAME "x86")
+	else()
+	  message(FATAL_ERROR "unhandled enumeration element")		
+	endif()
+	
+	# Adjust the archive output directory.
+	set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/products/${OUTPUTPLATFORM_FOLDER_NAME}/lib)
 
-# Adjust the library output directory.
-set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/products/${EX_CONFIGURATION_NAME}/${EX_PLATFORM_NAME}/lib)
+	# Adjust the library output directory.
+	set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/products/${OUTPUTPLATFORM_FOLDER_NAME}/lib)
 
-# Adjust the runtime output directory.
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/products/${EX_CONFIGURATION_NAME}/${EX_PLATFORM_NAME}/bin)
+	# Adjust the runtime output directory.
+	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/products/${OUTPUTPLATFORM_FOLDER_NAME}/bin)
+endif()
 
-# For multi-target generators like MSVC, we specify disjoint output paths for targets.
-foreach( OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES} )
-	# Suffix for CMake's configuration type specific output path variables.
-    string( TOUPPER ${OUTPUTCONFIG} OUTPUTCONFIG )
-	# The name of the configuration as used in the pathnames.
-    string( TOLOWER ${OUTPUTCONFIG} EX_CONFIGURATION_NAME )
-	message("configurating output paths for ${EX_CONFIGURATION_NAME}")
-    set( CMAKE_RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${CMAKE_BINARY_DIR}/products/${EX_CONFIGURATION_NAME}/${EX_PLATFORM_NAME}/bin )
-    set( CMAKE_LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${CMAKE_BINARY_DIR}/products/${EX_CONFIGURATION_NAME}/${EX_PLATFORM_NAME}/lib )
-    set( CMAKE_ARCHIVE_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${CMAKE_BINARY_DIR}/products/${EX_CONFIGURATION_NAME}/${EX_PLATFORM_NAME}/lib )
-endforeach( OUTPUTCONFIG CMAKE_CONFIGURATION_TYPES )
+# This adds the module Google Test.
+macro(include_google_test)
+	# This ensures that Google Test is compiled against C++ 17.
+	# TODO: However, isn't it possible to simply use set(CMAKE_CXX_STANDARD 17)?
+	if (${IDLIB_CXX_COMPILER_ID} EQUAL ${IDLIB_CXX_COMPILER_ID_CLANG})
+	  set(CMAKE_CXX_FLAGS "-DGTEST_LANG_CXX11=1 -std=gnu++11")
+	else()
+	  set(CMAKE_CXX_FLAGS "-DGTEST_LANG_CXX11=1")
+	endif()
+	# Remember the Google Test directory.
+	if (NOT DEFINED IDLIB_PATH)
+	  set(googletest_dir "${CMAKE_CURRENT_SOURCE_DIR}/external/googletest")
+	else()
+	  set(googletest_dir "${IDLIB_PATH}/external/googletest")
+	endif()
+	# Add the Google Test directory.
+	add_subdirectory("${googletest_dir}" EXCLUDE_FROM_ALL)
+endmacro()
